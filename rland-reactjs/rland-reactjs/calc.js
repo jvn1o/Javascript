3df438a6f2e0cd6fac4c19192b26f8fae878c861
jvn1o <junok0987@naver.com>
2024-09-30 13:23:17 +0900

    Javascript

---
+++ rland-reactjs/rland-reactjs/calc.js
@@ -0,0 +1,57 @@
+class Calc extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            x:10,
+            y:20,
+            result:30
+        };
+    }
+
+    // xInputHandler(){};
+    yInputHandler(e){ this.setState({y:parseInt(e.target.value)}); }
+    calcHandler(){};
+
+    render() {
+        return <div>
+        <section id="calc" class="n-frame:rounded-shadow w:2/3">
+            <h1>계산기</h1>
+            <form class="n-form">
+                <div>
+                    <label>
+                        <span>X:</span>
+                        <input onChange={(e)=>{this.setState({x:parseInt(e.target.value)});}} type="text" value={this.state.x} class="text-align:right"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span>Y:</span>
+                        <input onChange={this.yInputHandler.bind(this)} type="text" value={this.state.y} class="text-align:right"/>
+                    </label>
+                </div>
+                <div>
+                    <input onClick={(e)=>{
+                        e.preventDefault();
+                        let x = this.state.x;
+                        let y = this.state.y;
+                        let result = x+y;
+                        this.setState({result});
+
+                    }} class="n-btn n-btn-color:main" type="submit" value="계산하기"/>
+                </div>
+                <div>
+                    <label>
+                        <span>Result:</span>
+                        <input type="text" value={this.state.result} readonly class="text-align:right"/>
+                    </label>
+                </div>
+            </form>
+        </section>
+    </div>
+    }
+}
+
+
+ReactDOM.render(<Calc/>, document.getElementById("root"));
